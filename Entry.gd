011d766b2434d5d859d975bc0e842e2c9416a8e0
aspirin2d <aspirin2d@outlook.com>
2021-04-27 17:58:55 +0800

    hat init

---
+++ Entry.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# var lb = Label.instance()
+	# lb.text = "Hello, World"
+	# add_child(lb)
+
+	print_debug("Hello, World!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass

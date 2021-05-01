471dd43d8b5cb89888da2c60ed3cd9caae57d6d1
aspirin2d <aspirin2d@outlook.com>
2021-05-01 18:54:58 +0800

    add grass script

--- World/Grass.gd
+++ World/Grass.gd
@@ -2,10 +2,14 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_attack"):
+		# create grass disapear effect
 		var GrassEffect = preload("res://Effects/GrassEffect.tscn") 
+
 		var ge = GrassEffect.instance();
 		ge.position = global_position
+
 		var world = get_tree().current_scene
 		world.add_child(ge)
+
 		queue_free()
 
